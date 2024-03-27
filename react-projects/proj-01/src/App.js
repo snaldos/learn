fcdc5ecafe11d9160645c341044424ab1f94d0c5
Arnaldo Lopes <arnaldoflopes@gmail.com>
2024-03-27 22:04:59 +0000

    new projects

--- react-projects/proj-01/src/App.js
+++ react-projects/proj-01/src/App.js
@@ -4,6 +4,9 @@ import RandomColor from "./components/random-color";
 import StarRating from "./components/star-rating";
 import ImageSlider from "./components/image-slider";
 import LoadMoreData from "./components/load-more-data";
+import TreeView from "./components/tree-view";
+import menus from "./components/tree-view/data";
+import QrCodeGenerator from "./components/qr-code-generator/"
 
 function App() {
   return (
@@ -21,8 +24,13 @@ function App() {
       {/* <ImageSlider url={"https://picsum.photos/v2/list/"} limit={"10"} page={"1"} /> */}
 
       {/* Load more products component */}
-      <LoadMoreData/>
+      {/* <LoadMoreData /> */}
 
+      {/* Load more products component / recursive navigation menu */}
+      {/* <TreeView menus={menus} /> */}
+
+      {/* QR Code Generator */}
+      <QrCodeGenerator/>
     </div>
   );
 }
